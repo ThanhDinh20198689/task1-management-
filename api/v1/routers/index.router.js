14be8308c3a18af200cf0feb9cbd8aa2cc58859a
ThanhDinh20198689 <150313284+ThanhDinh20198689@users.noreply.github.com>
2025-01-15 17:20:45 +0700

    second time

--- api/v1/routers/index.router.js
+++ api/v1/routers/index.router.js
@@ -4,6 +4,7 @@ const AuthMiddleware= require('../middleware/auth.middleware');
 module.exports= (app)=>{         //đây là cú phát common js có tác dụng giống với "export const RouteOfClient=()=>{} "
 
     const version = "/api/v1";
+    
    app.use(version + "/tasks",AuthMiddleware.requireAuth,TaskRouter);
 
    app.use(version + "/users",UserRouter);
